1972415dca85c66e8c0018645ea4435eb417e4e5
samasama <tilo@samasama.de>
2019-09-24 22:04:08 +0200

    Python 4/28

--- python/Section 4 - Lottery.py
+++ python/Section 4 - Lottery.py
@@ -1,22 +1,11 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> number = "5,16,25,3,4,1"
->>> number
-'5,16,25,3,4,1'
->>> number.split(",")
-['5', '16', '25', '3', '4', '1']
->>> user_numbers = number.split(",")
->>> user_numbers
-['5', '16', '25', '3', '4', '1']
->>> user_numbers_as_int = []
->>> for number in user_numbers:
-	user_numbers_as_int.append(int(number))
+def get_player_numbers():
+    number_csv = input("Enter your 6 numbers, seperated by commas: ")
+    # create set of integers from number_csv
+    number_list = number_csv.split(",")
+    integer_set = {int(number) for number in number_list}
+    return integer_set
 
-	
->>> user_numbers_as_int
-[5, 16, 25, 3, 4, 1]
->>> [number for number in user_numbers]
-['5', '16', '25', '3', '4', '1']
->>> [int(number) for number in user_numbers]
-[5, 16, 25, 3, 4, 1]
->>> 
+print(get_player_numbers())
+
+        
+    
