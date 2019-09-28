2080db34672a940d400a03c0a114f72d98e4ff13
samasama <tilo@samasama.de>
2019-09-28 21:56:10 +0200

    Python 29

--- python/Section 4 - Lottery.py
+++ python/Section 4 - Lottery.py
@@ -1,3 +1,6 @@
+import random
+
+#User can pick 6 numbrs
 def get_player_numbers():
     number_csv = input("Enter your 6 numbers, seperated by commas: ")
     # create set of integers from number_csv
@@ -5,7 +8,14 @@ def get_player_numbers():
     integer_set = {int(number) for number in number_list}
     return integer_set
 
-print(get_player_numbers())
+#calculate 6 random numbers
+def create_lottery_numbers():
+    values = set()
+    while len(values) < 6:
+        values.add(random.randint(1,20))
+    return values    
+    
+print(create_lottery_numbers())
 
         
     
