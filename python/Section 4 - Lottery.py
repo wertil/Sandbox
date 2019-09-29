7ac3fdc50a6ca948654ebf93519c7cae5ca6b1d4
samasama <tilo@samasama.de>
2019-09-29 22:00:34 +0200

    python section 4

--- python/Section 4 - Lottery.py
+++ python/Section 4 - Lottery.py
@@ -1,5 +1,16 @@
 import random
 
+def menu():
+    #Ask player for numbers
+    user_numbers = get_player_numbers()
+    
+    #calculate lottery numbers
+    lottery_numbers = create_lottery_numbers()
+    
+    #print out the winnings
+    matched_numbers = user_numbers.intersection(lottery_numbers)
+    print("You matched {}. You won ${}!".format(matched_numbers,100**len(matched_numbers)))
+
 #User can pick 6 numbrs
 def get_player_numbers():
     number_csv = input("Enter your 6 numbers, seperated by commas: ")
@@ -13,9 +24,11 @@ def create_lottery_numbers():
     values = set()
     while len(values) < 6:
         values.add(random.randint(1,20))
-    return values    
+    return values
+
+#match numbers with lottery numbers
     
-print(create_lottery_numbers())
+menu()
 
         
     
