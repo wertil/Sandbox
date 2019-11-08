0f08920b2e28af5e57ef844ffd0a4740bbf8b93f
T‎ilo <werner.t@sportresult.com>
2019-11-08 16:55:45 +0100

    js webpack setup

--- complete-javascript-course-master/9-forkify/starter/webpack.config.js
+++ complete-javascript-course-master/9-forkify/starter/webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWepackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-        entry: './src/js/index.js',
-        output: {
-                path: path.resolve(__dirname, 'dist/js'),
-                filename: 'bundle.js'
-        }        
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWepackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
