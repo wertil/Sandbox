3bf886b6119bbade29a2bfa6e7f99f142ab1a99e
T‎ilo <werner.t@sportresult.com>
2019-11-22 16:56:07 +0100

    javascript

--- complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
+++ complete-javascript-course-master/9-forkify/starter/src/js/views/searchView.js
@@ -12,15 +12,29 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 }
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > 17) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
-    const markup = `
+    const markup = ` 
     <li>
         <a class="results__link" href="#${recipe.recipe_id}">
             <figure class="results__fig">
-                <img src="${recipe.image_url}" alt="${recipe.title}<">
+                <img src="${recipe.image_url}" alt="${limitRecipeTitle(recipe.title)}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
