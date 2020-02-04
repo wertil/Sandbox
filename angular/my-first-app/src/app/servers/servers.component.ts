2d0db57b3d6b6c632693265f63452ea31c867ab9
T‎ilo <werner.t@sportresult.com>
2020-02-04 15:08:55 +0100

    angular

---
+++ angular/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent implements OnInit {
+
+  allowNewServer = false;
+  serverCreationStatus = "no server was created";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "server was created!"
+  }
+
+}
