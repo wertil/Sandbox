f9bd00b2877258f457c4ee196e8455c442866105
samasama <tilo@samasama.de>
2020-02-23 22:26:26 +0100

    javascript

--- javascript/node/test.js
+++ javascript/node/test.js
@@ -1,12 +1,34 @@
-let http = require("http")
-let ourApp = http.createServer((req, res) => {
-    if (req.url == "/") {
-        res.end("Hello, and welcome to our website.")
-    }
-    if(req.url == "/about") {
-        res.end("Thank you.")
-    }
+let express = require("express")
+let ourApp = express()
 
-    res.end("We cand find the page you are looking for.")
+ourApp.use(express.urlencoded({extended: false}))
+
+ourApp.get('/', (req, res) => {
+    res.send(`
+        <form action="/answer" method="POST">
+        <p>What color is the sky?</p>
+        <input name="skyColor">
+        <button>Submit Answer</button>
+        </form>
+    `)
 })
+
+ourApp.post('/answer', (req, res) => {
+    if(req.body.skyColor.toUpperCase() == "BLUE"){
+        res.send(`
+            <p>congrats, that is correct</p>
+            <a href="/"> back to home</a>    
+        `)
+    } else {
+        res.send(`
+        <p>sorry, wrong answer</p>
+        <a href="/"> back to home</a>    
+    `)
+    }
+})
+
+ourApp.get('/answer', (req, res) => {
+    res.send("are you lost?")
+})
+
 ourApp.listen(3000)
