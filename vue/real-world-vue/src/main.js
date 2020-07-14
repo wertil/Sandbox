5f3871867d6bd34c929a8fc5571f11877743d25d
samasama <tilo@samasama.de>
2020-07-14 22:53:19 +0200

    vue slots

--- vue/real-world-vue/src/main.js
+++ vue/real-world-vue/src/main.js
@@ -3,7 +3,39 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
 Vue.config.productionTip = false
+
+// Manual component import
+// import BaseIcon from '@/components/BaseIcon.vue';
+//Vue.component('BaseIcon', BaseIcon);
+
+// Automatic Component import:
+import upperFirst from 'lodash/upperFirst' 
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    )
+  )
+
+  Vue.component(
+    componentName,
+    componentConfig.default || componentConfig
+  )
+}) 
+
+
 
 new Vue({
   router,
