1fcfe316e025b743d45da2f88675a0aed1c88074
samasama <tilo@samasama.de>
2020-09-21 22:34:54 +0200

    js slide in on scroll, nuxt e6

--- vue/real-world-nuxt/pages/index.vue
+++ vue/real-world-nuxt/pages/index.vue
@@ -1,18 +1,35 @@
 <template>
     <div>
         <h1>Events</h1>
+        <EventCard v-for="(event, index) in events" :key="index" :event="event" :data-index="index"/>
     </div>
 </template>
 
 <script>
 
 
+import EventCard from "~/components/EventCard";
+
 export default {
+    components: {EventCard},
     // method for vue-meta
     head() {
         return {
             title: 'Event Listing '
         }
+    },
+    // $axios destructured out of the context object
+    asyncData({$axios, error}) {
+        return $axios.get('http://localhost:3333/events').then(response => {
+            return {
+                events: response.data
+            }
+        }).catch(e => {
+            error({
+                statusCode: 503,
+                message: 'Unable to fetch events. Please try again later.'
+            })
+        })
     }
 
 }
