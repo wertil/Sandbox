c70446f0837574aba2c0e5ee0ae5d78230d5468e
samasama <tilo@samasama.de>
2020-10-04 22:02:05 +0200

    javascript location

---
+++ javascript/tutorials/Javacscript30/19 - Webcam Fun/scripts.js
@@ -0,0 +1,19 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream)
+            video.src = window.URL.createObjectURL(localMediaStream)
+            video.play()
+        })
+        .catch(error => {
+            console.log("Shit!", error);
+        })
+}
+
+getVideo()
