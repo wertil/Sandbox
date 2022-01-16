7e91da8b3c99471e999530578658c991ba3e7b92
samasama <tilo@samasama.de>
2022-01-16 23:22:38 +0100

    added primevue

--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.min.css',
+        'primeicons/primeicons.css'
+    ]
 })
